f504c73251cdd940a15de9358f255f9b835b2ea2
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-01-18 11:10:39 -0600

    Finished the first two

--- ThreeArrayProduct.java
+++ ThreeArrayProduct.java
@@ -1,6 +1,11 @@
+import org.w3c.dom.ranges.Range;
+
 public class ThreeArrayProduct {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
         
+        int answer = a[0]*a[1]*a[2];
+        System.out.println(answer);
     }
 }
+//javac ThreeArrayProduct.java;java ThreeArrayProduct
--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,22 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {5, 7, 9};
+        int num = a[0];
+        int less = a[0];
+        if(num<a[1]){
+            num = a[1];
+        }
+        if(num<a[2]){
+            num=a[2];
+        }
+        if(less>a[1]){
+            less = a[1];
+        }
+        if(less>a[2]){
+            less = a[2];
+        }
+        int ans = num-less;
+        System.out.println(ans);
     }
 }
+//javac ThreeArrayRange.java;java ThreeArrayRange
