38a6a804f3c8ac72cf2ef059e408b915460779df
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-01-18 11:38:59 -0600

    Finished these two

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,33 @@
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
-        
+        int[] a = {6, 2, 1};
+        int num =a[0];
+        int num2 =a[1];
+        int num3 = a[2];
+
+        int list =0;
+        int lis = 0;
+        if(num>num2){
+            list = num;
+            lis =num2;
+            num = lis;
+            num2 = list;
+        }
+        if(num>num3){
+            list = num;
+            lis =num3;
+            num = lis;
+            num3 = list; 
+        }
+        if(num2>num3){
+            list = num2;
+            lis =num3;
+            num2 = lis;
+            num3 = list; 
+        }
+        int[] b = {num,num2,num3};
+        System.out.println("Answer is "+b[0]+" "+b[1]+" "+b[2]);
+        System.out.println("From the list of "+a[0]+" "+a[1]+" "+a[2]);
     }
 }
+//javac ImmutableSort.java; java ImmutableSort
--- MutableSort.java
+++ MutableSort.java
@@ -1,5 +1,35 @@
 public class MutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {15, 20, 8};
+        int num =a[0];
+        int num2 =a[1];
+        int num3 = a[2];
+
+        int list =0;
+        int lis = 0;
+        if(num>num2){
+            list = num;
+            lis =num2;
+            num = lis;
+            num2 = list;
+        }
+        if(num>num3){
+            list = num;
+            lis =num3;
+            num = lis;
+            num3 = list; 
+        }
+        if(num2>num3){
+            list = num2;
+            lis =num3;
+            num2 = lis;
+            num3 = list; 
+        }
+        
+        a[0] = num;
+        a[1] = num2;
+        a[2] = num3;
+        System.out.println("Answer is "+a[0]+" "+a[1]+" "+a[2]);
     }
 }
+//javac MutableSort.java; java MutableSort
